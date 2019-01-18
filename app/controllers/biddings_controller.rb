ecb6894565e958d584bf849be57868d7e7105f1e
mateforlife <rodriguez.ruiz.alex@gmail.com>
2019-01-18 14:40:41 -0300

    feature: search form on biddings index

--- app/controllers/biddings_controller.rb
+++ app/controllers/biddings_controller.rb
@@ -5,15 +5,24 @@ class BiddingsController < ApplicationController
   before_action :set_market, only: %i[index more_info show]
 
   def index
-    @biddings = json_response(@market.select_biddings_with_key_words)
+    biddings = json_response(@market.biddings)
+    @biddings = params[:term].blank? ? biddings : search(biddings)
   end
 
-  def more_info
-    @biddings = @market.process_biddings
-  end
+  # def more_info
+  #   @biddings = @market.process_biddings
+  # end
 
   def show
     @bidding = json_response(@market.bidding_detail(params[:id]))[0]
+  end
+
+  def search(biddings)
+    arr = []
+    biddings.each do |bidding|
+      arr << bidding if I18n.transliterate(bidding[:Nombre].downcase).include?(params[:term].downcase)
+    end
+    arr
   end
 
   private
